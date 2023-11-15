34400a517967ddca37e66e392b6287f036a80d52
MOUSSA THIOR <netrathior@gmail.com>
2023-11-15 19:36:21 +0000

    Update source code

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import tkinter as tk
+from tkinter import ttk
 import random
 import time
 
-TIME_SLEEP = 0.6
+TIME_SLEEP = 0.4
+COLOR_LIFE = "yellow"
+COLOR_DEAD = "black"
+GRID_LINE_COLOR = "grey"
 
 
 class JeuDeLaVie:
@@ -14,10 +18,15 @@ class JeuDeLaVie:
         self.width = width
         self.height = height
         self.cell_size = cell_size
+
+        # Création d'un conteneur Frame pour organiser la grille, les boutons, et les explications.
+        self.frame = tk.Frame(self.root)
+        self.frame.pack()
+
         # Création d'un canvas pour afficher la grille.
-        self.canvas = tk.Canvas(self.root, width=width * cell_size, height=height * cell_size, borderwidth=0,
+        self.canvas = tk.Canvas(self.frame, width=width * cell_size, height=height * cell_size, borderwidth=0,
                                 highlightthickness=0)
-        self.canvas.pack()
+        self.canvas.grid(row=0, column=0, columnspan=4)
 
         # Initialisation de la grille à une matrice vide (toutes les cellules mortes).
         self.grid = [[0] * width for _ in range(height)]
@@ -25,20 +34,50 @@ class JeuDeLaVie:
         self.running = False
         # Configuration des boutons dans l'interface.
         self.setup_buttons()
+        # Configuration de l'événement de clic de la souris.
+        self.canvas.bind("<Button-1>", self.add_cell)
+
+        # Configuration de la zone d'explication.
+        self.setup_explanation()
 
     def setup_buttons(self):
         # Création des boutons Start, Stop, Restart, et Quit dans l'interface.
-        start_button = tk.Button(self.root, text="Start", command=self.start_game)
-        start_button.pack(side=tk.LEFT)
 
-        stop_button = tk.Button(self.root, text="Stop", command=self.stop_game)
-        stop_button.pack(side=tk.LEFT)
+        # Utilisation de la police et de la taille de police personnalisées.
+        button_font = ("Helvetica", 12)
 
-        restart_button = tk.Button(self.root, text="Restart", command=self.restart_game)
-        restart_button.pack(side=tk.LEFT)
+        # Utilisez ttk.Button pour des styles améliorés.
+        style = ttk.Style()
+        style.configure('TButton', font=button_font, padding=5, borderwidth=2, foreground='black', background='#4CAF50')
 
-        quit_button = tk.Button(self.root, text="Quit", command=self.root.destroy)
-        quit_button.pack(side=tk.RIGHT)
+        start_button = ttk.Button(self.frame, text="Start", command=self.start_game, style='TButton')
+        start_button.grid(row=1, column=0, padx=5, pady=5)
+
+        stop_button = ttk.Button(self.frame, text="Stop", command=self.stop_game, style='TButton')
+        stop_button.grid(row=1, column=1, padx=5, pady=5)
+
+        restart_button = ttk.Button(self.frame, text="Restart", command=self.restart_game, style='TButton')
+        restart_button.grid(row=1, column=2, padx=5, pady=5)
+
+        quit_button = ttk.Button(self.frame, text="Quit", command=self.root.destroy, style='TButton')
+        quit_button.grid(row=1, column=3, padx=5, pady=5)
+
+    def setup_explanation(self):
+        # Création d'une zone d'explication du jeu avec un design moderne.
+        explanation_text = """
+        Règles :
+        1. Une cellule morte avec exactement 3 voisins vivants devient vivante.
+        2. Une cellule vivante avec moins de 2 ou plus de 3 voisins vivants devient morte.
+
+        Cliquez sur la grille pour ajouter des cellules.
+        Utilisez les boutons pour contrôler le jeu.
+        """
+
+        explanation_frame = tk.Frame(self.frame, bg='#f0f0f0', pady=10)
+        explanation_frame.grid(row=2, column=0, columnspan=4, padx=10, pady=10, sticky='ew')
+
+        explanation_label = tk.Label(explanation_frame, text=explanation_text, justify=tk.LEFT, bg='#f0f0f0', fg='#333')
+        explanation_label.pack()
 
     def start_game(self):
         # Méthode appelée lorsqu'on clique sur le bouton Start.
@@ -98,13 +137,20 @@ class JeuDeLaVie:
         for y in range(self.height):
             for x in range(self.width):
                 if self.grid[y][x] == 1:
-                    color = "black"
+                    color = COLOR_LIFE
                 else:
-                    color = "white"
+                    color = COLOR_DEAD
                 # Dessine un rectangle pour représenter la cellule.
                 self.canvas.create_rectangle(x * self.cell_size, y * self.cell_size,
                                              (x + 1) * self.cell_size, (y + 1) * self.cell_size,
-                                             fill=color, outline="gray")
+                                             fill=color, outline=GRID_LINE_COLOR)
+
+    def add_cell(self, event):
+        # Méthode appelée lorsqu'on clique sur le canvas pour ajouter une cellule à la position du clic.
+        x = event.x // self.cell_size
+        y = event.y // self.cell_size
+        self.grid[y][x] = 1
+        self.draw_grid()
 
 
 if __name__ == "__main__":
